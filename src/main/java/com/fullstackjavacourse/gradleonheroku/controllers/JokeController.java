bf3f1dd9677459618d311ebb45a0cc3c2424d09a
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-08-11 18:56:40 +0200

    Fixed problem with accessing joke and register pages

--- src/main/java/com/fullstackjavacourse/gradleonheroku/controllers/JokeController.java
+++ src/main/java/com/fullstackjavacourse/gradleonheroku/controllers/JokeController.java
@@ -1,31 +1,36 @@
 package com.fullstackjavacourse.gradleonheroku.controllers;
-
 import com.fullstackjavacourse.gradleonheroku.GradleOnHerokuApplication;
 import com.fullstackjavacourse.gradleonheroku.models.Joke;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
-
-@RestController
+@Controller
 public class JokeController {
 
+    @GetMapping("/index")
+    public String index() {
+        return "/index";
+    }
+
     @GetMapping("/joke")
-    public static Joke getJoke() {
+    public String joke(Model model) {
         ArrayList<Joke> jokes = GradleOnHerokuApplication.getJokes();
 
         Random ran = new Random();
         int ranIndex = ran.nextInt(jokes.size());
-
-        return jokes.get(ranIndex);
+        Joke joke = jokes.get(ranIndex);
+        model.addAttribute("joke", joke);
+        return "/joke";
     }
 
-    @GetMapping("/joke/all")
-    public static ArrayList<Joke> getJokes() {
-        return GradleOnHerokuApplication.getJokes();
+    @GetMapping("/registered")
+    public String registered(@RequestParam(value = "email") String email, Model model) {
+        model.addAttribute(email);
+        return "/registered";
     }
-
 }
